069370d0711c9c0b78cdeb3ae2733311e5cf01ba
Mickelini <mickelini@users.noreply.github.com>
2020-12-02 02:29:29 -0500

    API
    Figuring where the API should go and how it should be called. Need to figure out data binding to canvas

--- game.js
+++ game.js
@@ -8,4 +8,26 @@ window.onload = function() {
     scene: [Scene1, Scene2],
   }
   let game = new Phaser.Game(config);
+  let api = new Sudoku();
+
+  let gameBoard = api.Start();
+  console.log(gameBoard);
+  
+}
+class Sudoku {
+  getSudokuPuzzle() {
+    return fetch(
+      'https://sugoku.herokuapp.com/board?difficulty=random'
+    )
+    .then((r) => r.json())
+    .then((r) => r.puzzle)
+  }
+  
+  async Start()
+  {
+    var puzzle = await this.getSudokuPuzzle();
+    console.log(puzzle);
+    return this.puzzle;
+    //Reset Gameboard here and draw out your puzzle with the api results.
+  }
 }
