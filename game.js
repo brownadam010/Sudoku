e1fabdf2bea7d8967d1572a479b20c38f3d006fb
RickTheWizzard <45324454+brownadam010@users.noreply.github.com>
2020-12-01 23:57:23 -0500

    making table for sudoku board

--- game.js
+++ game.js
@@ -1,26 +1,26 @@
-let config;
 
 window.onload = function() {
-  config = {
-    width: 1200,
-    height: 1200,
-    backgroundColor: 0xffffff,
-    scene: [Scene1, Scene2],
-  }
-  let game = new Phaser.Game(config);
+  
   let api = new Sudoku();
-
-  let gameBoard = api.Start();
-  console.log(gameBoard);
+  let gameboard = api.Start();
+  console.log(gameboard);
   
 }
+
+let gameDiv = document.getElementById('gameDiv');
+let playGame = document.getElementById('playGame');
+let gameBoard = document.getElementById('game');
+let game;
+
 class Sudoku {
+
   getSudokuPuzzle() {
     return fetch(
       'https://sugoku.herokuapp.com/board?difficulty=random'
     )
     .then((r) => r.json())
     .then((puzzle) => {
+      console.log(puzzle);
       return puzzle;
     })
     
@@ -28,9 +28,30 @@ class Sudoku {
   
   async Start()
   {
-    var puzzle = await this.getSudokuPuzzle();
+    let puzzle = await this.getSudokuPuzzle();
     console.log(puzzle);
     return this.puzzle;
     //Reset Gameboard here and draw out your puzzle with the api results.
   }
+
+  //add response to sudoku board with populated values.
+  /*generateBoard() {
+    for (let i = 0; i < gameBoard.rows.length; i++) {
+      for (let x = 0; x < gameBoard.rows[i].cells.length; x++) {
+        let start = this.getSudokuPuzzle();
+        console.log(start);
+        if (start[i][x] != "0") {
+          gameBoard.rows[i].cells[x].innerHTML = start[i][x];
+        }
+      }
+    }
+  }*/
+
 }
+
+playGame.addEventListener("click", function(clickEvent) {
+  //game = new Sudoku();
+  //game.generateBoard();
+  gameDiv.classList.remove("hidden");
+});
+
