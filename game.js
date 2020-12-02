15e8c3768aac8921cf95fe6e2196df8c90db4e1e
Mickelini <mickelini@users.noreply.github.com>
2020-12-02 16:06:15 -0500

    Update game.js

--- game.js
+++ game.js
@@ -4,6 +4,10 @@ window.onload = function() {
   let api = new Sudoku();
   let gameboard = api.Start();
   console.log(gameboard);
+=======
+
+  api.Start();
+>>>>>>> Stashed changes
   
 }
 
@@ -28,8 +32,32 @@ class Sudoku {
   
   async Start()
   {
+<<<<<<< Updated upstream
     let puzzle = await this.getSudokuPuzzle();
     console.log(puzzle);
+=======
+    var puzzle = await this.getSudokuPuzzle();
+    
+    var line1 = puzzle.board[0];
+    var line2 = puzzle.board[1];
+    var line3 = puzzle.board[2];
+    var line4 = puzzle.board[3];
+    var line5 = puzzle.board[4];
+    var line6 = puzzle.board[5];
+    var line7 = puzzle.board[6];
+    var line8 = puzzle.board[7];
+    var line9 = puzzle.board[8];
+    //Putting Each line in a variable and printing them for reference.
+    console.log(line1);
+    console.log(line2);
+    console.log(line3);
+    console.log(line4);
+    console.log(line5);
+    console.log(line6);
+    console.log(line7);
+    console.log(line8);
+    console.log(line9);
+>>>>>>> Stashed changes
     return this.puzzle;
     //Reset Gameboard here and draw out your puzzle with the api results.
   }
